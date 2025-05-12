47f946d7df7c8ed9e5b1cd6509f37526edba5896
Yan Chagas <yanchagas2004@gmail.com>
2025-05-12 12:00:32 -0300

    start: Ambiente preparado

---
+++ atividade-iv-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-gradient-to-br from-blue-600 to-blue-900">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
