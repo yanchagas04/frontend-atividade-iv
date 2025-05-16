e26f036b95f9a849abcdfd5c38ae11617c707717
Yan Chagas <yanchagas2004@gmail.com>
2025-05-16 14:38:02 -0300

    feat: Adicionando animções e SVG

--- atividade-iv-frontend/src/components/Login/login.tsx
+++ atividade-iv-frontend/src/components/Login/login.tsx
@@ -14,12 +14,16 @@ export default function Login() {
         type: type
     });
     return (
-        <div id="div-login" className="w-10/12 md:w-1/2 max-w-md h-fit flex flex-col justify-center items-center bg-gray-200 p-4 gap-4 rounded-lg drop-shadow-2xl dm-serif" onChange={() => {
+        <div id="div-login" className="w-10/12 md:w-1/2 max-w-md h-fit flex flex-col justify-center items-center bg-gray-200 p-4 gap-4 rounded-lg drop-shadow-2xl inter" onChange={() => {
             if (document.getElementById('aviso-formulario')) {
                 document.getElementById('aviso-formulario')?.remove();
             }
         }}>
-            <h1 className="text-3xl font-bold w-fit">Novo Usuário</h1>
+            <div className="w-full h-fit flex flex-col justify-center items-center gap-2">
+
+                <svg  xmlns="http://www.w3.org/2000/svg"  width={64}  height={64}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-movie"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                <h1 className="text-3xl font-bold w-fit">Novo Usuário</h1>
+            </div>
             <div className="w-full h-fit flex flex-col justify-center items-center gap-1">
                 <label htmlFor="name" className="text-center w-full flex">Nome</label>
                 <input id="name" type="text" className="w-full p-2 bg-gray-300 rounded-sm"/>
@@ -36,7 +40,7 @@ export default function Login() {
                     analyze_password();
                 }}/>
             </div>
-            <button className="w-full text-white p-2 bg-green-600 hover:bg-green-700 font-bold rounded-sm transition duration-150 ease-in-out cursor-pointer" onClick={async () => {
+            <button className="w-full text-white p-2 bg-green-600 hover:bg-green-700 font-bold rounded-sm transition duration-150 ease-in-out cursor-pointer hover:scale-[102%]" onClick={async () => {
                 const response = await analyze_forms();
                 console.log(response);
                 if (response.success) {
@@ -45,6 +49,7 @@ export default function Login() {
                     notify(response.msg, 'error');
                 }
             }}>Registrar</button>
+            <a href="/" className="w-full text-center hover:text-gray-700 transition-all duration-150 ease-in-out">Já tem uma conta? Clique aqui para logar.</a>
             <ToastContainer />
         </div>
     )
